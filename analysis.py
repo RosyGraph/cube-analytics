e972ca5f6053d7789c13f6daeadd288d89f7e2df
Chris Stevenson <chris.stevenson@live.com>
2021-01-31 22:25:51 -0700

    rework analysis to use card rank (needs refactor)

--- analysis.py
+++ analysis.py
@@ -10,36 +10,54 @@ def print_quartiles(cards):
     upper_median = sum(upper_half) / len(upper_half)
     fourths = []
     fourths.append(
-        [
-            cardname
-            for cardname, info in cards.items()
-            if info[0] >= upper_median
-        ]
+        sorted(
+            [
+                cardname
+                for cardname, info in cards.items()
+                if info[0] >= upper_median
+            ],
+            key=lambda item: item[0],
+        )
     )
     fourths.append(
-        [
-            cardname
-            for cardname, info in cards.items()
-            if median < info[0] < upper_median
-        ]
+        sorted(
+            [
+                cardname
+                for cardname, info in cards.items()
+                if median < info[0] < upper_median
+            ],
+            key=lambda item: item[0],
+        )
     )
     fourths.append(
-        [
-            cardname
-            for cardname, info in cards.items()
-            if lower_median < info[0] <= median
-        ]
+        sorted(
+            [
+                cardname
+                for cardname, info in cards.items()
+                if lower_median < info[0] <= median
+            ],
+            key=lambda item: item[0],
+        )
     )
     fourths.append(
-        [
-            cardname
-            for cardname, info in cards.items()
-            if info[1] and info[0] < lower_median
-        ]
+        sorted(
+            [
+                cardname
+                for cardname, info in cards.items()
+                if info[1] and info[0] < lower_median
+            ],
+            key=lambda item: item[0],
+        )
     )
+    rank = 1
+    worst_weight = 1
     for i, cardnames in enumerate(fourths):
         tier = i + 1
         print(f"tier {tier} cards:")
-        for cardname in sorted(cardnames):
-            print(f"\t{cardname}")
+        sorted_fourth = sorted(cardnames, key=lambda item: -cards[item][0])
+        for cardname in sorted_fourth:
+            if cards[cardname][0] < worst_weight:
+                rank += 1
+                worst_weight = cards[cardname][0]
+            print("{:<5}{:<65}".format(str(rank), cardname))
         print()
